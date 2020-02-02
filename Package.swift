90a70083253c887bd4158f1d6d3055bb74c93e8a
Aryan Ghassemi <aryan.ghassemi@creditkarma.com>
2020-02-01 18:30:25 -0800

    no message

--- Package.swift
+++ Package.swift
@@ -2,5 +2,11 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPoet",
-    exclude: ["Tests/SwiftPoet/FullGeneratorTests"]
-)
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [.library(name: "SwiftPoet", targets: ["SwiftPoet"])],
+    targets: [.target(name: "SwiftPoet", path: "Sources")], swiftLanguageVersions: [.v5])
